010fb98caa8964c55238b998b3bacf60498d4b4f
Brandon Wylie <brawyl@Brandons-MBP-2018.local>
2018-11-01 20:10:11 -0600

    Adds functions for deleting a temp order fully and loading a previous order.

---
+++ BudHubServer/clear_temp.php
@@ -0,0 +1,13 @@
+<?php
+$credentials = parse_ini_file("config.ini");
+$user = $credentials["user"];
+$password = $credentials["password"];
+$con = new mysqli("localhost",$user,$password,"budhubdb");
+
+$id = $_GET["id"];
+
+$st = $con->prepare("delete from temporder where uid=?");
+$st->bind_param("s",$id);
+$st->execute();
+$rs = $st->get_result();
+echo "1";
---
+++ BudHubServer/load_temp.php
@@ -0,0 +1,25 @@
+<?php
+$credentials = parse_ini_file("config.ini");
+$user = $credentials["user"];
+$password = $credentials["password"];
+$con = new mysqli("localhost",$user,$password,"budhubdb");
+
+$order = $_GET["order"];
+$uid = $_GET["uid"];
+
+$orders = explode(";", $order);
+foreach($orders as $item) {
+    $items = explode(":", $item);
+    $pid = $items[0];
+    if (strlen($pid) > 0) {
+        $qty = "1";
+    
+        $lid = $pid.$uid;
+
+        $st_check = $con->prepare("insert into temporder values(?,?,?,?)");
+        $st_check->bind_param("ssss", $pid, $uid, $qty, $lid);
+        $st_check->execute();
+    }
+}
+
+echo "1";
