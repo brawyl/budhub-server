0e4d2fa5e0dbcf4fbc8aae2bb5410a620acfc2bd
Brandon Wylie <brawyl@Brandons-MBP-2018.local>
2018-11-02 18:30:47 -0600

    Checks for keyword to clear the order and not load a new one in temporder.

--- BudHubServer/load_temp.php
+++ BudHubServer/load_temp.php
@@ -11,18 +11,20 @@ $st = $con->prepare("delete from temporder where uid=?");
 $st->bind_param("s",$uid);
 $st->execute();
 
-$orders = explode(";", $order);
-foreach($orders as $item) {
-    $items = explode(":", $item);
-    $pid = $items[0];
-    if (strlen($pid) > 0) {
-        $qty = "1";
+if (strpos($order, "clear") === false) {
+    $orders = explode(";", $order);
+    foreach($orders as $item) {
+        $items = explode(":", $item);
+        $pid = $items[0];
+        if (strlen($pid) > 0) {
+            $qty = "1";
     
-        $lid = $pid.$uid;
+            $lid = $pid.$uid;
 
-        $st_check = $con->prepare("insert into temporder values(?,?,?,?)");
-        $st_check->bind_param("ssss", $pid, $uid, $qty, $lid);
-        $st_check->execute();
+            $st_check = $con->prepare("insert into temporder values(?,?,?,?)");
+            $st_check->bind_param("ssss", $pid, $uid, $qty, $lid);
+            $st_check->execute();
+        }
     }
 }
 
